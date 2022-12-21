742871265ba1c24ae843aef2d7970954e2b08612
MokshaShah <moksha@simformsolutions.com>
2022-12-21 19:02:01 +0530

    vuetify added

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import i18n from './i18n'
 import router from './router'
+import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 
 new Vue({
   i18n,
   router,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
