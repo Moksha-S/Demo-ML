62037091a5a700fd8cfeea96368d9688c51f93cb
MokshaShah <moksha@simformsolutions.com>
2022-12-21 14:50:31 +0530

    changes in json server

---
+++ db.js
@@ -0,0 +1,18 @@
+const ar = require('./src/locales/ar.json');
+
+const en = require('./src/locales/en.json');
+
+// Something more
+
+
+ 
+
+module.exports = () => ({
+
+  ar: ar,
+
+  en:en
+
+  // Something more
+
+});
