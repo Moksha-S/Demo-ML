92a4939cec9d7ecc0804622f821c9aeaa87e5310
MokshaShah <moksha@simformsolutions.com>
2023-01-04 12:20:39 +0530

    changes in edit box

--- src/views/HomeMl.vue
+++ src/views/HomeMl.vue
@@ -3,9 +3,32 @@
     <h1 class="main-title">
       <textHover fieldName="title"></textHover>
     </h1>
-    <h2 v-hoverCar>{{ $t("welcomeString") }}</h2>
+
+    <h2
+      v-hoverCar="'demo'"
+      @mouseover="onTextHover($event, 'welcomeString')"
+      @mouseleave="onTextleave()"
+    >
+      {{ $t("welcomeString") }}
+      <v-btn
+        icon
+        v-if="onHover && editFieldName == 'welcomeString'"
+        @click="onBtnClick()"
+      >
+        <v-icon>mdi-pencil</v-icon>
+      </v-btn>
+    </h2>
+
     <div style="margin: 4% 10%">
       <CarCards />
+    </div>
+
+    <div v-if="showModal">
+      <EditBox
+        :editShow="showModal"
+        :fieldName="editFieldName"
+        @update="changeShowModal()"
+      />
     </div>
   </div>
 </template>
@@ -14,17 +37,22 @@
 import axios from "axios";
 import CarCards from "@/components/CarCards.vue";
 import textHover from "@/components/textHover.vue";
+import EditBox from "@/components/EditBox.vue";
 
 export default {
   name: "HomeView",
   components: {
     CarCards,
     textHover,
+    EditBox,
   },
   data() {
-    return {};
+    return {
+      onHover: false,
+      showModal: false,
+      editFieldName: "",
+    };
   },
-
   created() {
     axios
       .get(`http://localhost:3000/${this.$i18n.locale}`)
@@ -36,7 +64,28 @@ export default {
       });
   },
 
-  methods: {},
+  methods: {
+    changeShowModal() {
+      this.showModal = !this.showModal;
+    },
+
+    onBtnClick() {
+      this.showModal = !this.showModal;
+    },
+
+    onTextHover(evt, str) {
+      // console.log(evt.target.innerHTML);
+      this.editFieldName = str;
+      this.onHover = true;
+      // setTimeout(() => {
+        // this.showModal = !this.showModal;
+      // }, 2000);
+    },
+    
+    onTextleave() {
+      this.onHover = false;
+    },
+  },
 };
 </script>
 
@@ -47,5 +96,10 @@ export default {
 .main-title {
   display: flex;
   justify-content: center;
+}
+.demo-btn {
+  position: absolute;
+  top: 5em;
+  left: 63%;
 }
 </style>
